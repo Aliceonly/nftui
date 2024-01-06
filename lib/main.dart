a55e44b91300906fc1f68bdefd8a353ab5df31fc
Aliceonly <aliceonly@foxmail.com>
2024-01-06 16:14:45 +0800

    init: scaffold

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:nftui/screens/onboarding_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Dsignes'),
+      home: OnBoardingScreen(),
+    );
+  }
+}
---
+++ lib/screens/onboarding_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  final double _padding = 40;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [],
+      ),
+    );
+  }
+}
